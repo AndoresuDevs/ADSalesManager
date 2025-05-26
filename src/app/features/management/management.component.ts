8e47f64daf0b6723549e5334b02524e89a510b35
Andres Dimas <andresdmtz@hotmail.com>
2025-05-25 22:13:31 -0600

    Management child navigation

--- src/app/features/management/management.component.ts
+++ src/app/features/management/management.component.ts
@@ -1,8 +1,10 @@
-import { Component } from '@angular/core';
+import { Component} from '@angular/core';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-management',
-  imports: [],
+  standalone: true,
+  imports: [RouterModule],
   templateUrl: './management.component.html',
   styleUrl: './management.component.scss'
 })
